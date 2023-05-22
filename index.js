835d19b1b4cd532bef7f8b76a2a564321a2e75ac
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-05-22 18:20:46 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 19
+let num4 = 5
+
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+const mod = num3 % num4 
+
+const max = Math.max(1, 3, 6, 12, 20)
+
